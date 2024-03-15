c932522b6046a11f00a15d99ec71f1a050bb028c
jaehan <kyobo.jaehan@gmail.com>
2024-03-15 16:48:28 +0900

    next-auth 콜백함수 수정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -15,61 +15,54 @@ const handler = NextAuth({
             clientSecret: process.env.NAVER_CLIENT_SECRET!
         }),
         CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                username: { label: '이메일', type: 'text', placeholder: '이메일 주소를 입력해 주세요.' },
-                password: { label: '비밀번호', type: 'password' },
-            },
-            async authorize(credentials, req) {
-                const res = await fetch(`${process.env.NEXTAUTH_URL}/api/signin`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        username: credentials?.username,
-                        password: credentials?.password,
-                    }),
-                })
-                const user = await res.json()
-                console.log('$$$user: ', user)
+          name: 'Credentials',
+          credentials: {
+            username: { label: '이메일', type: 'text', placeholder: '이메일 주소를 입력해 주세요.' },
+            password: { label: '비밀번호', type: 'password' },
+          },
 
-                if (user) {
-                    // Any object returned will be saved in `user` property of the JWT
-                    return user
-                } else {
-                    // If you return null then an error will be displayed advising the user to check their details.
-                    return null
+          async authorize(credentials, req) {
+            const res = await fetch(`${process.env.NEXTAUTH_URL}/api/signin`, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  username: credentials?.username,
+                  password: credentials?.password,
+              }),
+            })
 
-                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                }
+            const user = await res.json()
 
-            },
+            if (user) {
+                // Any object returned will be saved in `user` property of the JWT
+                return user
+            } else {
+                // If you return null then an error will be displayed advising the user to check their details.
+                return null
+
+                // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+            }
+
+          },
         })
     ],
     callbacks:{
 
-        /**
-         * JWT Callback
-         * 웹 토큰이 실행 혹은 업데이트 될 때마다 콜백이 실행
-         * 반환된 값은 암호화되어 쿠키에 저장됨
-         */
+      // token: Next.js JWT 토큰 , user: authorize() 반환값
+      async jwt({ token, user }) {
 
-        async jwt({ token, user, account }) {
-            return { ...token, ...user};
-        },
-        /**
-         * Session Callback
-         * ClientSide에서 NextAuth에 세션을 체크할때마다 실행
-         * 반환된 값은 useSession을 통해 ClientSide에서 사용할 수 있음
-         * JWT 토큰의 정보를 Session에 유지 시킨다.
-         */
-        async session({ session, token }) {
-            console.log('$$$ token: ', token)
-            session.user = token as any;
-            console.log('$$$ session: ', session)
-            return session;
-        },
+        // 리턴된 값은 토큰에 저장된다.
+        return { ...token, ...user };
+      },
+
+      // session: next-auth session , token: Next.js JWT 토큰
+      async session({ session, token }) {
+        session.user = token as any;
+
+        return session;
+      },
     },
 
     // pages:{
